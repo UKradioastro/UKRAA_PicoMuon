b2547112b809ecdd9cd249f8a03e73038af652f9
Richard Knott <r.knott@outlook.com>
2025-02-13 11:58:16 +0000

    Updated install script to create file structure

---
+++ install/install.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+
+echo "Start installing UKRAA PicoMuon software..."
+
+
+echo "Start installing pandas software..."
+sudo apt install python3-pandas
+echo "pandas software installed"
+
+
+echo "Start installing gnuplot software..."
+sudo apt-get install gnuplot
+sudo apt-get install gnuplot-doc
+sudo apt-get install gnuplot-x11
+echo "gnuplot software installed"
+
+
+echo "Creating directory structure..."
+mkdir -vp data/neutrons
+mkdir -vp data/processed/adc/ACM0
+mkdir -vp data/processed/cpm/ACM0
+mkdir -vp data/processed/day/ACM0
+mkdir -vp data/processed/month/ACM0
+mkdir -vp data/processed/week/ACM0
+mkdir -vp data/processed/month/ACM0
+mkdir -v logfiles
+mkdir -vp plots/adc/ACM0
+mkdir -vp plots/cpm/ACM0
+mkdir -vp plots/day/ACM0
+mkdir -vp plots/freq/ACM0
+mkdir -vp plots/month/ACM0
+mkdir -vp plots/week/ACM0
+mkdir -v temp
+echo "Directory structure created"
+
+
+echo "Start installing PicoMuonACM0.service..."
+sudo cp -f /home/pi/UKRAA_PicoMuon/install/PicoMuonACM0.service /etc/systemd/system
+sudo chmod 644 /etc/systemd/system/PicoMuonACM0.service
+sudo systemctl daemon-reload
+sudo systemctl enable PicoMuonACM0.service
+sudo systemctl start PicoMuonACM0.service
+echo "PicoMuonACM0.service installed and started"
+
+
+echo "Start installing PicoMuon crontab entry..."
+sudo crontab -l -u root | cat - /home/pi/UKRAA_PicoMuon/install/crontab-PicoMuonACM0-entry.cron | crontab -u root -
+echo "PicoMuon crontab entry installed"
+
+echo "Start installing web server on RPi..."
+
+echo "Update packages..."
+sudo apt-get update
+
+echo "Install apache2..."
+sudo apt-get install apache2 -y
+
+echo "Install php..."
+sudo apt-get install php libapache2-mod-php -y
+
+echo "Install mariadb..."
+sudo apt-get install mariadb-server
+sudo mysql_secure_installation
+
+echo "Install the php-mysql connector..."
+sudo apt install php-mysql
+
+echo "Restart apache2..."
+sudo service apache2 restart
+echo "web server installed on RPi"
+
+
+echo "Move files to /var/www/html..."
+sudo cp /home/pi/UKRAA_PicoMuon/WWW/index.html /var/www/html/index.html
+sudo cp -r /home/pi/UKRAA_PicoMuon/WWW/images /var/www/html/
+sudo cp -r /home/pi/UKRAA_PicoMuon/WWW/temp /var/www/html/
+echo "Files moved to /var/www/html"
+
+
+echo "Start installing webpage update crontab entry..."
+sudo crontab -l -u root | cat - /home/pi/UKRAA_PicoMuon/install/crontab-webpage-update-entry.cron | crontab -u root -
+echo "webpage update crontab entry installed"
+
+
+echo "Completed installing UKRAA PicoMuon software."
+
+
+echo "Checking PicoMuonACM0.service running"
+sudo systemctl status PicoMuonACM0.service
