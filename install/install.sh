90485870ca6d2108ba1cdc5b73aa5783bdf8becc
Richard Knott <r.knott@outlook.com>
2025-09-30 14:41:02 +0100

    more simple changes

--- install/install.sh
+++ install/install.sh
@@ -3,6 +3,9 @@
 
 echo "Start installing UKRAA PicoMuon software..."
 
+echo "What is your username?"
+read -p 'Username:' uservar
+
 
 echo "Start installing pandas software..."
 apt install python3-pandas -y
@@ -17,34 +20,34 @@ echo "gnuplot software installed"
 
 
 echo "Creating directory structure..."
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/data/environment/coefficient
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/data/NMDB/neutrons
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/data/processed/3month/ACM0
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/data/processed/adc/ACM0
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/data/processed/cpm/ACM0
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/data/processed/day/ACM0
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/data/processed/month/ACM0
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/data/processed/week/ACM0
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/data/raw/ACM0
-sudo -u $USER mkdir -v  /home/$USER/UKRAA_PicoMuon/logfiles
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/plots/3month/ACM0
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/plots/adc/ACM0
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/plots/cpm/ACM0
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/plots/day/ACM0
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/plots/freq/ACM0
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/plots/month/ACM0
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/plots/week/ACM0
-sudo -u $USER mkdir -v  /home/$USER/UKRAA_PicoMuon/temp
-sudo -u $USER mkdir -vp /home/$USER/UKRAA_PicoMuon/WWW/temp
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/data/environment/coefficient
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/data/NMDB/neutrons
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/data/processed/3month/ACM0
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/data/processed/adc/ACM0
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/data/processed/cpm/ACM0
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/data/processed/day/ACM0
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/data/processed/month/ACM0
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/data/processed/week/ACM0
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/data/raw/ACM0
+sudo -u uservar mkdir -v  /home/uservar/UKRAA_PicoMuon/logfiles
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/plots/3month/ACM0
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/plots/adc/ACM0
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/plots/cpm/ACM0
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/plots/day/ACM0
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/plots/freq/ACM0
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/plots/month/ACM0
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/plots/week/ACM0
+sudo -u uservar mkdir -v  /home/uservar/UKRAA_PicoMuon/temp
+sudo -u uservar mkdir -vp /home/uservar/UKRAA_PicoMuon/WWW/temp
 echo "Directory structure created"
 
 echo "Sort out file permissions..."
-sudo -u $USER chmod -v +x /home/$USER/UKRAA_PicoMuon/scripts/*.py
-sudo -u $USER chmod -v +x /home/$USER/UKRAA_PicoMuon/scripts/*.sh
+sudo -u uservar chmod -v +x /home/uservar/UKRAA_PicoMuon/scripts/*.py
+sudo -u uservar chmod -v +x /home/uservar/UKRAA_PicoMuon/scripts/*.sh
 echo "File permissions sorted out"
 
 echo "Start installing PicoMuonACM0.service..."
-cp -vf /home/$USER/UKRAA_PicoMuon/install/PicoMuonACM0.service /etc/systemd/system
+cp -vf /home/uservar/UKRAA_PicoMuon/install/PicoMuonACM0.service /etc/systemd/system
 chmod -v 644 /etc/systemd/system/PicoMuonACM0.service
 systemctl daemon-reload
 systemctl enable PicoMuonACM0.service
@@ -56,7 +59,7 @@ echo "Start installing PicoMuon crontab entry..."
 echo "Clearing current crontab entry..."
 echo "NOTE: if you have edited your sudo crontab - this will be deleted.  You will need to reedit sudo crontab post update."
 sudo crontab -u root -r
-crontab -u root -l  | cat - /home/$USER/UKRAA_PicoMuon/install/crontab-PicoMuonACM0-entry.cron | crontab -u root -
+crontab -u root -l  | cat - /home/uservar/UKRAA_PicoMuon/install/crontab-PicoMuonACM0-entry.cron | crontab -u root -
 echo "PicoMuon crontab entry installed"
 
 echo "Start installing web server on RPi..."
@@ -83,22 +86,22 @@ echo "web server installed on RPi"
 
 
 echo "Move files to /var/www/html..."
-cp -v /home/$USER/UKRAA_PicoMuon/WWW/index.html /var/www/html/index.html
-cp -vr /home/$USER/UKRAA_PicoMuon/WWW/images /var/www/html/
-cp -vr /home/$USER/UKRAA_PicoMuon/WWW/temp /var/www/html/
+cp -v /home/uservar/UKRAA_PicoMuon/WWW/index.html /var/www/html/index.html
+cp -vr /home/uservar/UKRAA_PicoMuon/WWW/images /var/www/html/
+cp -vr /home/uservar/UKRAA_PicoMuon/WWW/temp /var/www/html/
 echo "Files moved to /var/www/html"
 
 
 echo "Start installing webpage update crontab entry..."
-crontab -u root -l | cat - /home/$USER/UKRAA_PicoMuon/install/crontab-webpage-update-entry.cron | crontab -u root -
+crontab -u root -l | cat - /home/uservar/UKRAA_PicoMuon/install/crontab-webpage-update-entry.cron | crontab -u root -
 echo "webpage update crontab entry installed"
 
 
 echo "Final cleanup..."
-sudo -u $USER rm -vrf /home/$USER/UKRAA_PicoMuon/images
-sudo -u $USER rm -vrf /home/$USER/UKRAA_PicoMuon/WWW
-sudo -u $USER rm -vrf /home/$USER/UKRAA_PicoMuon/update
-sudo -u $USER rm -v /home/$USER/UKRAA_PicoMuon/README.md
+sudo -u uservar rm -vrf /home/uservar/UKRAA_PicoMuon/images
+sudo -u uservar rm -vrf /home/uservar/UKRAA_PicoMuon/WWW
+sudo -u uservar rm -vrf /home/uservar/UKRAA_PicoMuon/update
+sudo -u uservar rm -v /home/uservar/UKRAA_PicoMuon/README.md
 echo "Finished final cleanup"
 
 
@@ -107,4 +110,4 @@ sleep 10
 
 # if successful
 echo "Removing install directory and exiting..."
-exec rm -vrf /home/$USER/UKRAA_PicoMuon/install
+exec rm -vrf /home/uservar/UKRAA_PicoMuon/install
